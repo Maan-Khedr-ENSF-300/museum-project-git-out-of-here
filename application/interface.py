3b27f8a5d6cf016e734049edb2529856d1b7ff81
zahwafatima <zahwa.fatima003@gmail.com>
2022-12-06 17:46:09 -0700

    login while loop

--- application/interface.py
+++ application/interface.py
@@ -10,25 +10,40 @@ cnx = mysql.connector.connect(
 cur = cnx.cursor()
 
 # login
-username = input('Username: ')
-password = input('Password: ')
+cur.execute('use museum')
+cur.execute('SELECT * FROM USERS') # execute query to get all users
+users = cur.fetchall() # list of users (tuples)
+
+print('Welcome to the ENSF 300 Museum Project Database. Please login below:')
+status = True
+
+while status == True:
+    username = input('Username: ')
+    password = input('Password: ')
+
+    # simultaneously check if user exists and their account type
+    if (username, password, 'admin') in users: # if user is admin
+        # add users
+
+        # edit users
+
+        # block users
+
+        # change database
+
+    elif (username, password, 'data entry') in users: # if user is data entry type
+        # add information tuples (info meets contraints)
+
+        # modify existing information in database
+
+    elif (username, password, 'end') in users: # if user is end type
 
 
-# if admin 
-    # add users
-
-    # edit users
-
-    # block users
-
-    # change database
-
-# if data entry user 
-    # add information tuples (info meets contraints)
-
-    # modify existing information in database
-
-# if end user
+    else: # user is not valid
+        print('User does not exist.')
+        if input('Enter 1 to exit: ') == 1:
+            print('Thank you for using the ENSF 300 Museum Project Database.')
+            status = False
 
 
 cnx.close()
